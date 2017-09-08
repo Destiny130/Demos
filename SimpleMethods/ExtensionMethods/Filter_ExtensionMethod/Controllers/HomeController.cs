495b5bf21b109177249dc4dccacf6c9c6032dd8b
Destiny130 <destiny1307654641@hotmail.com>
2017-09-08 14:04:09 +0800

    Filter_ExtensionMethod

---
+++ SimpleMethods/ExtensionMethods/Filter_ExtensionMethod/Controllers/HomeController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using LanguageFeatures.Models;
+
+namespace LanguageFeatures.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult Result()
+        {
+            IEnumerable<Product> cart = new ShoppingCart
+            {
+                Products = new List<Product>
+                {
+                    new Product{Name="Kayak",Category="Watersports",Price=275M },
+                    new Product {Name = "Lifejacket",Category="Watersports", Price = 48.95M},
+                    new Product {Name = "Soccer ball",Category="Soccer", Price = 19.50M},
+                    new Product {Name = "Corner flag",Category="Soccer", Price = 34.95M}
+                }
+            };
+
+            decimal total = 0;
+            foreach (Product prod in cart.FilterByCategory("Soccer"))
+            {
+                total += prod.Price;
+            }
+            return View((object)string.Format("Cart Total: {0:c}", total));
+        }
+    }
+}
---
+++ SimpleMethods/ExtensionMethods/Filter_ExtensionMethod/Models/ExtensionMethods.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LanguageFeatures.Models
+{
+    public static class ExtensionMethods
+    {
+        public static decimal TotalPrices(this IEnumerable<Product> productEnum)
+        {
+            decimal total = 0;
+            foreach (Product pro in productEnum)
+            {
+                total += pro.Price;
+            }
+            return total;
+        }
+
+        public static IEnumerable<Product> FilterByCategory(this IEnumerable<Product> productEnum, string categoryParam)
+        {
+            foreach (Product prod in productEnum)
+            {
+                if (prod.Category == categoryParam)
+                {
+                    yield return prod;
+                }
+            }
+        }
+    }
+}
