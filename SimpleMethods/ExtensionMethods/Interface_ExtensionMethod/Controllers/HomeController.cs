05dec54d7d5d914d21a6ceefb921a00d8c89d690
Destiny130 <destiny1307654641@hotmail.com>
2017-09-08 13:08:23 +0800

    update

--- SimpleMethods/ExtensionMethods/Interface_ExtensionMethod/Controllers/HomeController.cs
+++ SimpleMethods/ExtensionMethods/Interface_ExtensionMethod/Controllers/HomeController.cs
@@ -40,8 +40,18 @@ namespace LanguageFeatures.Controllers
                     new Product {Name = "Corner flag", Price = 34.95M}
                 }
             };
+
+            Product[] productArray =
+            {
+                new Product{Name="Kayak",Price=275M },
+                    new Product {Name = "Lifejacket", Price = 48.95M},
+                    new Product {Name = "Soccer ball", Price = 19.50M},
+                    new Product {Name = "Corner flag", Price = 34.95M}
+            };
+
             decimal cartTotal = cart.TotalPrices();
-            return View((object)string.Format("Total: {0:c}", cartTotal));
+            decimal arrayTotal = productArray.TotalPrices();
+            return View((object)string.Format("Cart Total: {0:c}, Array Total: {1:c}", cartTotal, arrayTotal));
         }
     }
 }
