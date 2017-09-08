a12819935a4bc52f2fadeb828cb7c604342346b9
Destiny130 <destiny1307654641@hotmail.com>
2017-09-08 12:42:57 +0800

    change

---
+++ SimpleMethods/ExtensionMethods/ExtensionMethod/Models/ExtensionMethods.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LanguageFeatures.Models
+{
+    public static class ExtensionMethods
+    {
+        public static decimal TotalPrices(this ShoppingCart cartParam)
+        {
+            decimal total = 0;
+            foreach (Product pro in cartParam.Products)
+            {
+                total += pro.Price;
+            }
+            return total;
+        }
+    }
+}
--- SimpleMethods/ExtensionMethods/ExtensionMethod/Models/Product.cs
+++ SimpleMethods/ExtensionMethods/ExtensionMethod/Models/Product.cs
@@ -3,25 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 
-namespace LanguageFeatures.Models {
-
-    public class Product {
+namespace LanguageFeatures.Models
+{
+    public class Product
+    {
         private string name;
-
         public int ProductID { get; set; }
-
-        //public string Name {
-        //    get {
-        //        return ProductID + name;
-        //    }
-        //    set {
-        //        name = value;
-        //    }
-        //}
-        public string Name { get; set; }
-            
+        public string Name
+        {
+            get { return ProductID + name; }
+            set { name = value; }
+        }
         public string Description { get; set; }
         public decimal Price { get; set; }
-        public string Category { set; get; }
+        public string Category { get; set; }
     }
 }
