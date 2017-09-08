e3aa7bff775511feb939690155c86183e3b6fa2b
Destiny130 <destiny1307654641@hotmail.com>
2017-09-08 11:03:19 +0800

    ExtensionMethodDemo

---
+++ SimpleMethods/ExtensionMethod.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace test.Models
+{
+    public class ShoppingCart
+    {
+        public List<string> stringList { get; set; }
+    }
+
+    public static class MyExtensionMethod
+    {
+        public static decimal TotalLength(this ShoppingCart cartParam)
+        {
+            decimal total = 0;
+            foreach (string s in cartParam)
+            {
+                total += s.Length;
+            }
+            return total;
+        }
+    }
+}
