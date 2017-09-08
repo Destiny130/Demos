4dd90158743adae22e1d5289f2aa1a68849105b2
Destiny130 <destiny1307654641@hotmail.com>
2017-09-08 12:56:48 +0800

    Interface_ExtensionMethod

---
+++ SimpleMethods/ExtensionMethods/Interface_ExtensionMethod/Controllers/HomeController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using LanguageFeatures.Models;
+
+namespace LanguageFeatures.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult Result()
+        {
+            ShoppingCart cart = new ShoppingCart
+            {
+                Products = new List<Product>
+                {
+                    new Product{Name="Kayak",Price=275M },
+                    new Product {Name = "Lifejacket", Price = 48.95M},
+                    new Product {Name = "Soccer ball", Price = 19.50M},
+                    new Product {Name = "Corner flag", Price = 34.95M}
+                }
+            };
+            decimal cartTotal = cart.TotalPrices();
+            return View((object)string.Format("Total: {0:c}", cartTotal));
+        }
+    }
+}
---
+++ SimpleMethods/ExtensionMethods/Interface_ExtensionMethod/Models/ExtensionMethods.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LanguageFeatures.Models
+{
+    public static class ExtensionMethods
+    {
+        public static decimal TotalPrices(this IEnumerable<Product> productEnum)
+        {
+            decimal total = 0;
+            foreach (Product pro in productEnum)
+            {
+                total += pro.Price;
+            }
+            return total;
+        }
+    }
+}
